9212b52107e26f1d130c6f4b0ad74e89d835d47f
jkachhad <jalpeshk@gmail.com>
2024-06-09 14:01:56 -0700

    Introduction to Stormhalter

---
+++ Books/StormhalterIntroduction.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Kesmai.Server.Engines.Gumps;
+using Kesmai.Server.Engines.Library;
+using Image = Kesmai.Server.Engines.Gumps.Image;
+
+namespace Kesmai.Publications;
+
+public class StormhalterIntroduction() : PublishedBook(
+	uniqueId: "stormhalter-introduction",
+	
+	title: "Stormhalter",
+	author: "Luxx",
+	
+	new CompositePage(() =>
+    {
+	    var list = new List<GumpControl>()
+	    {
+		    new Image()
+		    {
+			    Texture = @"UI/Textures/Books/Image-Round-Dragon",
+			    Width = 450, Height = 450,
+			    HorizontalAlignment = HorizontalAlignment.Center,
+		    },
+		    
+		    new TextBlock()
+		    {
+			    Text = "Stormhalter",
+			    HorizontalAlignment = HorizontalAlignment.Center,
+				
+			    FontSize = 72, Font = "King",
+			    
+			    Margin = new Rectangle(0, 30, 0, 0)
+			},
+		    new TextBlock()
+		    {
+			    Text = "Written by Luxx",
+			    HorizontalAlignment = HorizontalAlignment.Center,
+				
+			    FontSize = 32, Font = "RobotoBold",
+			    Stroke = Color.White, 
+			    
+			    Margin = new Rectangle(0, 20, 0, 0)
+		    },
+	    };
+
+	    return list;
+    }),
+	new Page(
+		new ParagraphPageElement("Stormhalter isn't just a game project; it's a passion project by a group of people. It began as a remake of Legends of Kesmai, with the goal of building a welcoming community. "),
+		new ParagraphPageElement("The aim is to welcome new players, especially those who have never experienced Island of Kesmai, Legends of Kesmai, or Lands of Kes.")
+	));
