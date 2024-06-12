1eb7e6109642d810aa464e6d0bd97b857d6d4b5f
jkachhad <jalpeshk@gmail.com>
2024-06-11 23:05:35 -0700

    Add documentation.

--- Books/StormhalterIntroduction.cs
+++ Books/StormhalterIntroduction.cs
@@ -7,15 +7,18 @@ using Image = Kesmai.Server.Engines.Gumps.Image;
 namespace Kesmai.Publications;
 
 public class StormhalterIntroduction() : PublishedBook(
+	/* every book must have a unique id to persistence clashing. */
 	uniqueId: "stormhalter-introduction",
 	
 	title: "Stormhalter",
 	author: "Luxx",
 	
+	/* a composite page allows ui elements to be laid out in any manner. */
 	new CompositePage(() =>
-    {
+	{
 	    var list = new List<GumpControl>()
 	    {
+		    /* show a centered image. */
 		    new Image()
 		    {
 			    Texture = @"UI/Textures/Books/Image-Round-Dragon",
@@ -23,6 +26,7 @@ public class StormhalterIntroduction() : PublishedBook(
 			    HorizontalAlignment = HorizontalAlignment.Center,
 		    },
 		    
+		    /* display the tile and author under. */
 		    new TextBlock()
 		    {
 			    Text = "Stormhalter",
@@ -45,8 +49,11 @@ public class StormhalterIntroduction() : PublishedBook(
 	    };
 
 	    return list;
-    }),
+	}),
+	
+	/* a new page with more structured elements. */
 	new Page(
+		/* paragraphs are laid out and wrapped. */
 		new ParagraphPageElement("Stormhalter isn't just a game project; it's a passion project by a group of people. It began as a remake of Legends of Kesmai, with the goal of building a welcoming community. "),
 		new ParagraphPageElement("The aim is to welcome new players, especially those who have never experienced Island of Kesmai, Legends of Kesmai, or Lands of Kes.")
 	));
